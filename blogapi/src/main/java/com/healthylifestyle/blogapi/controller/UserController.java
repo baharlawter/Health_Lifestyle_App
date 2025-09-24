8bfe25f8cd77613f5a39f5ac705858d63cd4f1d9
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-09-24 12:20:05 -0500

    updated UserController.java for singing up

--- blogapi/src/main/java/com/healthylifestyle/blogapi/controller/UserController.java
+++ blogapi/src/main/java/com/healthylifestyle/blogapi/controller/UserController.java
@@ -9,20 +9,33 @@ import com.healthylifestyle.blogapi.model.User;
 import com.healthylifestyle.blogapi.repository.UserRepository;
 
 @RestController
+//All endpoints
 @RequestMapping("/api/auth")
 public class UserController {
+    //repository to interavt with the users tabele in db
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
+//handles post request
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody Map<String, String> loginData) {
+        //finds username from db
         User user = userRepository.findByUsername(loginData.get("username"));
         if (user != null && user.getPassword().equals(loginData.get("password"))) {
             return ResponseEntity.ok(user);
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
+//handles postrequest
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        //checks if username already exists indb
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
+        }//if not saves in db
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
     }
 }
