0bfa9fe5f53e88ae4b07266c501d42face199c05
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-09-29 08:52:08 -0500

    created put and delete endpoint in the Comments controller.

--- blogapi/src/main/java/com/healthylifestyle/blogapi/controller/CommentsController.java
+++ blogapi/src/main/java/com/healthylifestyle/blogapi/controller/CommentsController.java
@@ -2,6 +2,7 @@ package com.healthylifestyle.blogapi.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.healthylifestyle.blogapi.model.Comments;
@@ -27,4 +28,26 @@ public class CommentsController {
     public List<Comments> getAllComments(){
         return commentsRepository.findAll();
     }
+    // PUT: Update a comment by ID
+    @PutMapping("/{id}")
+    public Comments updateComment(@PathVariable Long id, @RequestBody Comments updatedComment) {
+        Comments comment = commentsRepository.findById(id).orElse(null);
+        if (comment == null) {
+            return null; 
+        }
+        comment.setText(updatedComment.getText());
+        comment.setRating(updatedComment.getRating());
+        return commentsRepository.save(comment);
+    }
+   // DELETE: Delete a comment by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity <String> deleteComment(@PathVariable Long id) {
+        if (!commentsRepository.existsById(id)){
+            return ResponseEntity.status(404).body("Comment not found")
+        }
+        commentsRepository.deleteById(id);
+        return ResponseEntity.ok("Comment deleted successfully :)!");
+    }
+
+
 }
